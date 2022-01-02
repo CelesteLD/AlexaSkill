d0aa5db5dff60b66a5828d85b5ad3e27ae33dbc9
alu0101321660 <72355426+alu0101321660@users.noreply.github.com>
2022-01-02 17:35:04 +0000

    Add files via upload

---
+++ lambda/question-list.js
@@ -0,0 +1,23 @@
+module.exports = {
+    '0' : {
+            'id' : '0',
+            'question' : 'Podrás elegir entre Tiburón, Mosquito y Nutria... ¿Cuál de estos animales tiene 47 dientes? ',
+            'result' : 'mosquito',
+            'answer' : 'Pese a su pequeño tamaño, tiene todos esos dientes.',
+            'clue' : 'Parecen muchos dientes,¿Verdad? No dejes que las apariencias te engañen...'
+        },
+        '1' : {
+            'id' : '1',
+            'question' : 'Su corazón pesa más de 11 Kilos. Elige entre Jirafa, Jaguar y Jabalí.',
+            'result' : 'jirafa',
+            'answer' : 'Aunque parezca demasiado pesado, el corazón de las jirafas puede llegar a pesar mas de 11 kilogramos.',
+            'clue' : 'Una de sus virtudes es que puede ver todo desde las alturas.'
+        },
+        '2' : {
+            'id' : '2',
+            'question' : '¿Cual es el animal más venenoso del mundo? Elige entre Medusa, Araña, Serpiente de cascabel.',
+            'result' : 'medusa',
+            'answer' : 'Este animal es el más venenoso del mundo aunque parezca sorprendente.',
+            'clue' : 'Puedes encontrarla en verano fácilmente.'
+        }
+};
